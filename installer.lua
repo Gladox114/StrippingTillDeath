4f2b90afd932968ad542f7d9c7c56256aa009bae
Gladox114 <Gladox114@github.com>
2021-03-07 23:28:06 +0100

    none

--- installer.lua
+++ installer.lua
@@ -1,4 +1,4 @@
 -- downloads the "github repository downloader", runs it and then downloads the StrippingTillDeath main repository
 -- http://www.computercraft.info/forums2/index.php?/topic/4072-github-repository-downloader/
 
-shell.run("pastebin run wPtGKMam github Gladox114 StrippingTillDeath / .")
+shell.run("pastebin run wPtGKMam Gladox114 StrippingTillDeath / .")
